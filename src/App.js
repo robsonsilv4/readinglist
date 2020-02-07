f269ab19f4d7e820dd230578e0c4d333a65c8001
robsonsilv4 <robsonsilv410@gmail.com>
2020-02-06 21:23:01 -0300

    Criado Context de autenticação

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Navbar from './components/navbar';
 import BookList from './components/bookList';
 import ThemeContextProvider from './contexts/themeContext';
 import ThemeToggle from './components/themeToggle';
+import AuthContextProvider from './contexts/authContext';
 
 function App() {
   return (
-    <ThemeContextProvider>
-      <div className="App">
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
-      </div>
-    </ThemeContextProvider>
+    <div className="App">
+      <ThemeContextProvider>
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
+      </ThemeContextProvider>
+    </div>
   );
 }
 
